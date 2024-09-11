90157239eafeaa997fde74cf5f5a9f17c25d7e7d
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-09-11 10:22:21 -0300

    upd: contraint validation

--- src/main/java/com/asaas/hackaton/dto/PaymentRequestDTO.java
+++ src/main/java/com/asaas/hackaton/dto/PaymentRequestDTO.java
@@ -1,10 +1,15 @@
 package com.asaas.hackaton.dto;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public record PaymentRequestDTO(
         String description,
         BigDecimal value,
         String dueDate
 ) {
+    public PaymentRequestDTO {
+        Objects.requireNonNull(value, "É necessário informar um valor");
+        Objects.requireNonNull(dueDate, "É necessário informar uma data de vencimento");
+    }
 }
