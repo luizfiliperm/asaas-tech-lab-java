ee976962a0090b7ff25f826bc4dc2860df2bcc80
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-09-12 14:59:34 -0300

    chore: trata erros de integracao

--- src/main/java/com/asaas/hackaton/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/asaas/hackaton/controller/GlobalControllerExceptionHandler.java
@@ -3,9 +3,11 @@ package com.asaas.hackaton.controller;
 import com.asaas.hackaton.exception.AccessNotPermittedException;
 import com.asaas.hackaton.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
@@ -18,5 +20,11 @@ class GlobalControllerExceptionHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessNotPermittedException.class)
     public void handleForbidden() {
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<String> httpClientError(HttpClientErrorException exception) {
+        return ResponseEntity.ok(exception.getResponseBodyAsString());
     }
 }
