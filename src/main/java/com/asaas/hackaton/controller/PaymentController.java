c2b4f687c70005a83d00525ffdb06d72e51236e8
Luiz Filipe <luizfilipemr17@gmail.com>
2024-10-01 16:53:07 -0300

    feat: add Idempotency to create payment endpoint

--- src/main/java/com/asaas/hackaton/controller/PaymentController.java
+++ src/main/java/com/asaas/hackaton/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.asaas.hackaton.burstlimit.BurstLimit;
 import com.asaas.hackaton.dto.ListPaymentResponseDTO;
 import com.asaas.hackaton.dto.PaymentRequestDTO;
 import com.asaas.hackaton.dto.PaymentResponseDTO;
+import com.asaas.hackaton.idempotency.Idempotent;
 import com.asaas.hackaton.quotalimit.QuotaLimit;
 import com.asaas.hackaton.ratelimit.RateLimit;
 import com.asaas.hackaton.service.PaymentService;
@@ -23,6 +24,7 @@ public class PaymentController {
     @PostMapping
     @QuotaLimit
     @BurstLimit(maxParallelRequests = 10)
+    @Idempotent
     public ResponseEntity<PaymentResponseDTO> save(@RequestBody PaymentRequestDTO paymentRequestDTO) {
         PaymentResponseDTO responseDTO = paymentService.create(paymentRequestDTO);
         return ResponseEntity.ok(responseDTO);
