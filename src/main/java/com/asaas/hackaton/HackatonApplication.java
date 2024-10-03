52bfa62f244679770732266abdac3188e6dbb079
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-10-03 10:12:15 -0300

    test

--- src/main/java/com/asaas/hackaton/HackatonApplication.java
+++ src/main/java/com/asaas/hackaton/HackatonApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class HackatonApplication {
 
 	public static void main(String[] args) {
+        System.out.println("test");
 		SpringApplication.run(HackatonApplication.class, args);
 	}
 
